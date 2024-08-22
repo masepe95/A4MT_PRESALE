22a79e22e60445cbcc6355987570d0911a6d7dd1
masepe95 <marcosepe10@gmail.com>
2024-08-22 09:53:19 +0200

    google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -47,8 +47,17 @@ export default defineNuxtConfig({
                 }
             }
         ],
-        "nuxt-swiper"
+        "nuxt-swiper",
+        '@nuxtjs/google-gtag', // Aggiunto modulo per Google Analytics
     ],
+    'google-gtag': {
+        id: 'G-GG7P21F453', // Sostituisci con il tuo Measurement ID di Google Analytics
+        config: {
+            anonymize_ip: true, // Anonimizza gli IP
+            send_page_view: true, // Invia la visualizzazione di pagina iniziale
+        },
+        debug: false, // Imposta su true per debug durante lo sviluppo
+    },
     plugins: [
         '~/plugins/bootstrap.client.js',
         '~/plugins/wow.client.js',
