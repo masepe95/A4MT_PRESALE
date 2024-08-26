0fd6557e4712c278149550282f888728e151ea9b
masepe95 <marcosepe10@gmail.com>
2024-08-26 11:01:01 +0200

    no google indic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,10 @@ export default defineNuxtConfig({
     },
     app: {
         head: {
-            meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }],
+            meta: [
+                { name: "viewport", content: "width=device-width, initial-scale=1" },
+                { name: "robots", content: "noindex" } // Indicizzazione google disattivata
+            ],
             title: "A4ManagementTools",
             script: [
                 {
